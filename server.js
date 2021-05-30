f910377af790c0b7507e1fc7d66b954b40bf81ed
edwarddevp <edwarddevp@gmail.com>
2021-05-30 15:11:29 -0400

    -Db production mode connection

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ const tasks = require('./controllers/tasks');
 
 const { verifyToken } = require('./middlewares/verifyToken');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: '123',
-    database: 'safe_event_planner_db'
+    connectionString : process.env.DATABASE_URL,
+    ssl:true
   }
 });
 
